a0b3689e2d4c8262179b882282b2f1ec75e21040
Eduardo Marques <marquesaedu@gmail.com>
2023-10-25 15:22:02 -0300

    knn e filtro de variancia baseado em mfcm implementados

---
+++ experiment.py
@@ -0,0 +1,108 @@
+import numpy as np
+import random
+from MFCM import MFCM
+from filters import *
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
+from sklearn.metrics import classification_report
+
+def execute(nRep, dataset, centersAll):
+	Jmin = 2147483647	# int_max do R
+	bestL = 0			# melhor valor de L_resp
+	bestM = 0			# melhor valor de M_resp
+	best_centers = 0
+
+	for r in range(nRep):
+		centers = list(map(int, centersAll[r,].tolist()))
+
+		resp = MFCM(dataset, centers, 2)
+
+		J = resp[0]
+		L_resp = resp[1]
+		M_resp = resp[2]
+
+		if (Jmin > J):
+			Jmin = J
+			bestL = L_resp
+			bestM = M_resp
+			best_centers = centers
+		
+	dict = {'Jmin': Jmin, 'bestL': bestL, 'bestM': bestM, 'best_centers': centersAll}
+	# Retorna os centers de todas as iterações para o KMeans (mudar para criar uma nova lista exclusiva para o KMeans)
+	
+	return dict
+
+def exec_mfcm_filter(dataset, nRep, nClusters):
+    ## Inicializando variáveis
+    result = {}
+    Jmin = 2147483647
+    centers = 0
+
+    data = np.vstack((dataset[0], dataset[1]))
+    ref = np.hstack((dataset[2], dataset[3]))
+
+    nObj = len(dataset)
+
+    centersMC = np.zeros((nRep, nClusters))
+
+    for c in range(nRep):
+        centersMC[c] = random.sample(range(1, nObj), nClusters)
+
+    clustering = execute(nRep, data, centersMC)
+
+    if clustering['Jmin'] < Jmin:
+        Jmin = clustering['Jmin']
+        result = clustering
+    centers = clustering['best_centers']
+
+    return result
+
+def run_filter(dataset, result, numVar, numClusters):
+	
+    data = np.vstack((dataset[0], dataset[1]))
+
+    resultado_filtro = variance_filter(data, result['bestM'], numClusters)
+    resultado_filtro[0].sort(key=lambda k : k[0])
+
+	## Aplicando filtro
+    data = apply_filter(data, resultado_filtro, numVar)
+
+    return data
+
+def select_dataset(indexData):
+    if indexData == 1:
+        data, target = load_iris(return_X_y=True)
+        data_train, data_test, target_train, target_test = train_test_split(data, target, test_size=0.3, random_state=0)
+        return (data_train, data_test, target_train, target_test)
+
+def exec_knn(data_train, data_test, target_train, target_test, n_neighbors):
+
+    clf = KNeighborsClassifier(n_neighbors=n_neighbors)
+    clf.fit(data_train, target_train)
+
+    target_pred = clf.predict(data_test)
+
+    print(classification_report(target_test, target_pred))
+
+if __name__ == "__main__":
+
+    n_neighbors = 3
+    numVar = 1
+    dataset = select_dataset(1)
+
+    result_mfcm = exec_mfcm_filter(dataset, 10, 3)
+
+    filtered_dataset = run_filter(dataset, result_mfcm, numVar, 3)
+
+    print('Executando KNN com dataset original')
+    r_data_train, r_data_test, r_target_train, r_target_test = dataset
+    exec_knn(r_data_train, r_data_test, r_target_train, r_target_test, n_neighbors)
+
+
+    # Melhorar o código na questão de datasets crus e filtrados
+    print('Executando KNN com dataset filtrado')
+    f_target_train = r_target_train
+    f_target_test = r_target_test
+    f_data_train, f_data_test = train_test_split(filtered_dataset, test_size=0.3, random_state=0)
+    exec_knn(f_data_train, f_data_test, f_target_train, f_target_test, n_neighbors)
---
+++ filters.py
@@ -0,0 +1,68 @@
+import numpy as np
+
+def sum_filter(data, U, nClusters):
+    V = []
+    nVar = data.shape[1]
+
+    for i in range(0, nVar):
+        aTotal = 0 # Relevância total
+        nObj = data.shape[0]
+
+        for j in range(0, nClusters):  
+            a = 0
+            for k in range(0, nObj):
+                u = U[i][k][j] # Acessa o grau de pertinência do objeto k ao cluster j em relação a variável i
+                a += u
+        
+            a /= nObj # Relevância em relação ao cluster j
+            aTotal += a
+
+        aTotal = round(aTotal * 100, 2)
+        V.append(aTotal)
+    
+    return (V, 'Filtro por Somatório')
+
+def variance_filter(data, U, nClusters):
+    V = []
+    nVar = data.shape[1]
+
+    for i in range(0, nVar):
+        # print(f'i: {i}')
+        aTotal = [] # Relevância total
+        nObj = data.shape[0]
+
+        for j in range(0, nClusters):  
+            a = []
+            for k in range(0, nObj):
+                u = U[i][k][j] # Acessa o grau de pertinência do objeto k ao cluster j em relação a variável i
+                a.append(u)
+        
+            a = np.asarray(a)
+            var = np.var(a)
+            aTotal.append(var)
+
+        aTotal = np.asarray(aTotal)
+        # print(aTotal)
+        media = np.mean(aTotal)
+
+        V.append((round(media * 100, 5), i))
+    
+    return (V, 'Filtro por Variância')
+
+
+def apply_filter(dataset, result, n):
+
+    result[0].sort(key=lambda k : k[0])
+
+    print(f'\n{result[1]}: ')
+    for item in range(len(result[0])):
+        print(f'Variável {result[0][item][1]}: {result[0][item][0]}')
+
+    listaCorte = [result[0][i][1] for i in range(n)]
+    dataset = np.delete(dataset, listaCorte, axis = 1)
+
+    print("\nVariáveis deletadas:")
+    for var in listaCorte:
+        print(f"Variável {var}")
+        
+    return dataset
